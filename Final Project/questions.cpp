c3294ba9e9b7f51687ac5a68f29e7ad19f2547fe
awong53 <awong9@hawaii.edu>
2018-04-17 15:13:41 -1000

    new text file for c++ questions

---
+++ Final Project/questions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class question{
+  private:
+    string q;
+  public:
+}
+
+class answer{
+  private:
+    string a1;
+    string a2;
+    string a3;
+    string a4;
+    int correct;
+  public:
+}

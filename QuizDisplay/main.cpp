f753c19614f57150ed0095e89744798e033d1ea0
howtotailslide <agasbarro36@hotmail.com>
2018-04-27 13:39:36 -1000

    Changed display system to sfml because QT is a piece of garbage

--- QuizDisplay/main.cpp
+++ QuizDisplay/main.cpp
@@ -1,11 +1,25 @@
-#include "mainwindow.h"
-#include <QApplication>
+#include <SFML/Graphics.hpp>
 
-int main(int argc, char *argv[])
+int main()
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    sf::Style::Resize;
+    shape.setFillColor(sf::Color::Green);
 
-    return a.exec();
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }

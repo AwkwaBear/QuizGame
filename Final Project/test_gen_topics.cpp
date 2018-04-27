45cf73da949dcdf7213ca5f8e440f3b429ebd705
howtotailslide <agasbarro36@hotmail.com>
2018-04-27 10:24:40 -1000

    update test driver to actually play quiz

--- Final Project/test_gen_topics.cpp
+++ Final Project/test_gen_topics.cpp
@@ -4,15 +4,24 @@
 #include "topics.hpp"
 
 int main(){
-
+//Generate Topic objects
   topic cpp;
-  std::cout << "main running" << std::endl;
-  cpp.generate_topic_cpp();
-  std::cout << "topic fully generated" << std::endl;
+  topic pop;
+  topic history;
+  topic hawaii;
+int input;
+//  std::cout << "main running" << std::endl;
+  //Initialize Topic objects and questions/answers associated
+//  cpp.generate_topic_cpp();
+  pop.generate_topic_pop();
+  history.generate_topic_history();
+  hawaii.generate_topic_hawaii();
+
+  std::cout << "topics fully generated" << std::endl;
 
   std::vector<question>::iterator it;
 
-  for(it = cpp.get_topic_begin(); it != cpp.get_topic_end(); it++){
+  for(it = pop.get_topic_begin(); it != pop.get_topic_end(); it++){
 
   //  std::cout << "for loop running" << std::endl;
     std::cout <<"Q: " << it->get_question() << std::endl;
@@ -21,7 +30,16 @@ int main(){
     std::cout <<"A3: " <<it->get_answer(3).read_answer() << std::endl;
     std::cout <<"A4: " <<it->get_answer(4).read_answer() << std::endl;
 
-    std::cout << std::endl;
+    std::cout << "Enter an answer: " << std::endl;
+
+    std::cin >> input;
+
+    if(it->get_answer(input).check_answer() == 1){
+      std::cout << "CORRECT!!" << std::endl;
+    }
+    else{
+      std::cout << "Answer is wrong" << std::endl;
+    }
 
   }
 
